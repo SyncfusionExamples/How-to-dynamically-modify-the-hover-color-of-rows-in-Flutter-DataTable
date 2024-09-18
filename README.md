c3fa204d518622e7a11ff730f97038f9316bb2a6
Abinesh <abinesh.sf3967@gmail.com>
2024-09-18 16:08:05 +0530

    KB changes

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# How-to-dynamically-modify-the-hover-color-of-rows-in-Flutter-DataTable
-How to dynamically modify the hover color of rows in Flutter DataTable
+# How to dynamically modify the hover color of rows in Flutter DataTable (SfDataGrid)?.
+
+In this article, we will show you how to dynamically modify the hover color of rows in [Flutter DataTable](https://www.syncfusion.com/flutter-widgets/flutter-datagrid).
+
+Initialize the [SfDataGrid](https://pub.dev/documentation/syncfusion_flutter_datagrid/latest/datagrid/SfDataGrid-class.html) widget with all the required properties. You can change the row highlighting color by using the [SfDataGridThemeData.rowHoverColor](https://pub.dev/documentation/syncfusion_flutter_core/latest/theme/SfDataGridThemeData/rowHoverColor.html) property. By wrapping the cells with a [MouseRegion](https://api.flutter.dev/flutter/widgets/MouseRegion-class.html), you can detect mouse enter and exit events to change the hover color. Use `setState` to update the rowHoverColor and trigger a rebuild of the SfDataGrid to reflect the changes.
+
+```dart
+  @override
+  void initState() {
+    super.initState();
+    employees = getEmployeeData();
+    employeeDataSource = EmployeeDataSource(setState, employeeData: employees);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Syncfusion Flutter DataGrid'),
+      ),
+      body: SfDataGridTheme(
+        data: SfDataGridThemeData(
+            rowHoverColor: employeeDataSource._rowHoverColor),
+        child: SfDataGrid(
+          source: employeeDataSource,
+          columnWidthMode: ColumnWidthMode.fill,
+          columns: getColumn(),
+        ),
+      ),
+    );
+  }
+
+class EmployeeDataSource extends DataGridSource {
+  EmployeeDataSource(this.setState, {required List<Employee> employeeData}) {
+   …..
+  }
+
+  List<DataGridRow> _employeeData = [];
+
+  StateSetter setState;
+
+  Color? _rowHoverColor;
+
+ …..
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    MaterialColor getColorBasedOnRowIndex(DataGridRow dataGridRow) {
+      int index = _employeeData.indexOf(dataGridRow);
+
+      if (index % 2 == 0 || index == 0) {
+        return Colors.amber;
+      } else if (index == 1) {
+        return Colors.purple;
+      } else if (index == 3) {
+        return Colors.pink;
+      } else if (index == 5) {
+        return Colors.teal;
+      } else if (index == 7) {
+        return Colors.cyan;
+      } else if (index == 9) {
+        return Colors.red;
+      } else {
+        return Colors.indigo;
+      }
+    }
+
+    return DataGridRowAdapter(
+        cells: row.getCells().map<Widget>((e) {
+      return MouseRegion(
+        onEnter: (event) {
+          setState(() {
+            _rowHoverColor = getColorBasedOnRowIndex(row);
+          });
+        },
+        onExit: (event) {
+          setState(() {
+            _rowHoverColor = null;
+          });
+        },
+        child: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8.0),
+          child: Text(e.value.toString()),
+        ),
+      );
+    }).toList());
+  }
+}
+```
+You can download this example on [GitHub](https://github.com/SyncfusionExamples/How-to-dynamically-modify-the-hover-color-of-rows-in-Flutter-DataTable).
---
+++ lib/main.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:syncfusion_flutter_core/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+/// The application that contains datagrid on it.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Syncfusion DataGrid Demo',
+      theme: ThemeData(useMaterial3: false),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+/// The home page of the application which hosts the datagrid.
+class MyHomePage extends StatefulWidget {
+  /// Creates the home page.
+  const MyHomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Employee> employees = <Employee>[];
+  late EmployeeDataSource employeeDataSource;
+
+  @override
+  void initState() {
+    super.initState();
+    employees = getEmployeeData();
+    employeeDataSource = EmployeeDataSource(setState, employeeData: employees);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Syncfusion Flutter DataGrid'),
+      ),
+      body: SfDataGridTheme(
+        data: SfDataGridThemeData(
+            rowHoverColor: employeeDataSource._rowHoverColor),
+        child: Center(
+          child: SizedBox(
+            width: 500,
+            height: 550,
+            child: SfDataGrid(
+              source: employeeDataSource,
+              columnWidthMode: ColumnWidthMode.fill,
+              columns: <GridColumn>[
+                GridColumn(
+                    columnName: 'id',
+                    label: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          'ID',
+                        ))),
+                GridColumn(
+                    columnName: 'name',
+                    label: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        alignment: Alignment.center,
+                        child: const Text('Name'))),
+                GridColumn(
+                    columnName: 'designation',
+                    label: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          'Designation',
+                          overflow: TextOverflow.ellipsis,
+                        ))),
+                GridColumn(
+                    columnName: 'salary',
+                    label: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        alignment: Alignment.center,
+                        child: const Text('Salary'))),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Employee> getEmployeeData() {
+    return [
+      Employee(10001, 'James', 'Project Lead', 20000),
+      Employee(10002, 'Kathryn', 'Manager', 30000),
+      Employee(10003, 'Lara', 'Developer', 15000),
+      Employee(10004, 'Michael', 'Designer', 15000),
+      Employee(10005, 'Martin', 'Developer', 15000),
+      Employee(10006, 'Newberry', 'Developer', 15000),
+      Employee(10007, 'Balnc', 'Developer', 15000),
+      Employee(10008, 'Perry', 'Developer', 15000),
+      Employee(10009, 'Gable', 'Developer', 15000),
+      Employee(10010, 'Grimes', 'Developer', 15000)
+    ];
+  }
+}
+
+/// Custom business object class which contains properties to hold the detailed
+/// information about the employee which will be rendered in datagrid.
+class Employee {
+  /// Creates the employee class with required details.
+  Employee(this.id, this.name, this.designation, this.salary);
+
+  /// Id of an employee.
+  final int id;
+
+  /// Name of an employee.
+  final String name;
+
+  /// Designation of an employee.
+  final String designation;
+
+  /// Salary of an employee.
+  final int salary;
+}
+
+/// An object to set the employee collection data source to the datagrid. This
+/// is used to map the employee data to the datagrid widget.
+class EmployeeDataSource extends DataGridSource {
+  /// Creates the employee data source class with required details.
+  EmployeeDataSource(this.setState, {required List<Employee> employeeData}) {
+    _employeeData = employeeData
+        .map<DataGridRow>((e) => DataGridRow(cells: [
+              DataGridCell<int>(columnName: 'id', value: e.id),
+              DataGridCell<String>(columnName: 'name', value: e.name),
+              DataGridCell<String>(
+                  columnName: 'designation', value: e.designation),
+              DataGridCell<int>(columnName: 'salary', value: e.salary),
+            ]))
+        .toList();
+  }
+
+  List<DataGridRow> _employeeData = [];
+
+  StateSetter setState;
+
+  Color? _rowHoverColor;
+
+  @override
+  List<DataGridRow> get rows => _employeeData;
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    MaterialColor getColorBasedOnRowIndex(DataGridRow dataGridRow) {
+      int index = _employeeData.indexOf(dataGridRow);
+
+      if (index % 2 == 0 || index == 0) {
+        return Colors.amber;
+      } else if (index == 1) {
+        return Colors.purple;
+      } else if (index == 3) {
+        return Colors.pink;
+      } else if (index == 5) {
+        return Colors.teal;
+      } else if (index == 7) {
+        return Colors.cyan;
+      } else if (index == 9) {
+        return Colors.red;
+      } else {
+        return Colors.indigo;
+      }
+    }
+
+    return DataGridRowAdapter(
+        cells: row.getCells().map<Widget>((e) {
+      return MouseRegion(
+        onEnter: (event) {
+          setState(() {
+            _rowHoverColor = getColorBasedOnRowIndex(row);
+          });
+        },
+        onExit: (event) {
+          setState(() {
+            _rowHoverColor = null;
+          });
+        },
+        child: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8.0),
+          child: Text(e.value.toString()),
+        ),
+      );
+    }).toList());
+  }
+}
